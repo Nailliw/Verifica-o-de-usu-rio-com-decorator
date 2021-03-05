22dcc9d74a605425cd5ca0c72d8520506ee3807a
Willian Ribeiro <willianribeirodev@gmail.com>
2021-03-04 23:25:58 -0300

    add register_user function

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ def create_file(filename):
 
 
 filename = 'users.csv'
-create_file(filename)
+
+
+def proximo_id(filename):
+    id = []
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f)
+        for line in reader:
+            id.append(int(line['id']))
+    
+    return sorted(id)[-1] +1 if id != [] else 1
 
 
 def all_users(filename):
@@ -30,5 +39,21 @@ def all_users(filename):
         return (characters, id[-1])
 
 todos = all_users(filename)
-print(todos)
 
+
+def register_user(filename, **kwargs):
+    with open(filename, 'a') as f:
+        id = {'id': proximo_id(filename)}
+        id.update(kwargs)
+
+        collumns = ['id','nome', 'email', 'password']
+        writer = csv.DictWriter(f, fieldnames=collumns)
+        if not os.stat(filename).st_size:
+            writer.writeheader()
+
+        writer.writerow(id)
+
+    return id
+
+user_registered = register_user(filename, **{'nome': 'jose', 'email': "jose@hotmail.com", "password": '1234'})
+print(user_registered)
